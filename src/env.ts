872734c3688dc688ddac9a4ba4604d9633842ece
cyborgM1002 <mkchauhan0543@gmail.com>
2024-02-15 23:59:44 +0530

    [PLO] P-10-underdevelopment, created project page

---
+++ src/env.ts
@@ -0,0 +1,1 @@
+export const GITHUB_URL = "https://api.github.com/users/cyborgM1002/repos";
---
+++ src/pages/projects/project-items/ProjectItems.tsx
@@ -0,0 +1,6 @@
+// const ProjectItems = (items) => {
+//   console.log(items);
+//   return <div className="w-40 h-40"></div>;
+// };
+
+// export default ProjectItems;
