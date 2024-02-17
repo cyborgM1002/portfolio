34f7bb94a2aefb670ae428675fa2a9da01b92d99
cyborgM1002 <mkchauhan0543@gmail.com>
2024-02-18 04:07:32 +0530

    changed rout and xlogo and nav icons

--- src/components/navbar/navbar-items/NavbarItems.tsx
+++ src/components/navbar/navbar-items/NavbarItems.tsx
@@ -1,26 +1,29 @@
 import { Link } from "react-router-dom";
 import { CapitalizeFirstLetter } from "../../../utils/TextCapitalizer";
 import { GrHomeRounded } from "react-icons/gr";
-import { FaRegCircleUser } from "react-icons/fa6";
-import { SiOpenproject } from "react-icons/si";
-import { FaProjectDiagram } from "react-icons/fa";
+import { GiSkills } from "react-icons/gi";
+import { FaAppStore } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
 
 function NavbarIcon(value: string) {
   switch (value) {
     case "home":
       return <GrHomeRounded />;
-    case "about":
-      return <FaRegCircleUser />;
-    case "contact":
-      return <FaProjectDiagram />;
+    case "skills":
+      return <GiSkills />;
+    case "experience":
+      return <FaCode />;
     case "projects":
-      return <SiOpenproject />;
+      return <FaAppStore />;
   }
 }
 
 const NavbarItems = ({ title }: { title: string }) => {
   return (
-    <Link className="hover:text-blue-700" to={title === "home" ? "/" : title}>
+    <Link
+      className="hover:text-[rgb(0,223,192)]"
+      to={title === "home" ? "/" : title}
+    >
       {CapitalizeFirstLetter(title)}
     </Link>
   );
