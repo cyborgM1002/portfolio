6c79b95b7f6697b1aaed105da9c0b2fb9e0a2cef
cyborgM1002 <mkchauhan0543@gmail.com>
2024-02-19 04:42:53 +0530

    [PLO] P-12 UPDT/Navbar icons

--- src/components/navbar/navbar-items/NavbarItems.tsx
+++ src/components/navbar/navbar-items/NavbarItems.tsx
@@ -1,20 +1,20 @@
 import { Link } from "react-router-dom";
 import { CapitalizeFirstLetter } from "../../../utils/TextCapitalizer";
-import { GrHomeRounded } from "react-icons/gr";
-import { GiSkills } from "react-icons/gi";
-import { FaAppStore } from "react-icons/fa";
-import { FaCode } from "react-icons/fa";
+import ProjectsIcon from "/icons/ProjectsIcon.png";
+import ExperienceIcon from "/icons/ExperienceIcon.png";
+import SkillIcon from "/icons/SkillIcon.png";
+import HomeIcon from "/icons/HomeIcon.png";
 
 function NavbarIcon(value: string) {
   switch (value) {
     case "home":
-      return <GrHomeRounded />;
-    case "skills":
-      return <GiSkills />;
+      return HomeIcon;
+    case "skill":
+      return SkillIcon;
     case "experience":
-      return <FaCode />;
+      return ExperienceIcon;
     case "projects":
-      return <FaAppStore />;
+      return ProjectsIcon;
   }
 }
 
@@ -30,7 +30,15 @@ const NavbarItems = ({ title }: { title: string }) => {
 };
 
 export const MobileNavbarItems = ({ title }: { title: string }) => {
-  return <Link to={title === "home" ? "/" : title}>{NavbarIcon(title)}</Link>;
+  return (
+    <Link to={title === "home" ? "/" : title}>
+      <img
+        className="md:w-14 md:h-14 w-10 h-10"
+        src={NavbarIcon(title)}
+        alt=""
+      />
+    </Link>
+  );
 };
 
 export default NavbarItems;
