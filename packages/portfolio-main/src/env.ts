c39eb5176a53c2cb574c4b74bbd1f967f1bb1fa9
bugg-m <mkchauhan0543@gmail.com>
2024-03-20 20:53:19 +0530

    WIP: [PLO] P-20 Integrated to single-spa application

---
+++ packages/portfolio-main/src/env.ts
@@ -0,0 +1,10 @@
+const config = {
+  GITHUB_API_URL: "https://api.github.com/users/bugg-m",
+  GITHUB_URL: "https://github.com/bugg-m",
+  GITHUB_API_REPO_URL: "https://api.github.com/users/bugg-m/repos",
+  USER_SUMMARY:
+    "As a dedicated MERN (MongoDB, Express.js, React.js, Node.js) developer, I am passionate about crafting robust and dynamic web applications that deliver exceptional user experiences. With a solid foundation in full-stack development and a keen eye for detail, I thrive in collaborative environments where I can leverage my expertise to drive innovation and achieve project success.",
+};
+
+export const { GITHUB_URL, USER_SUMMARY, GITHUB_API_URL, GITHUB_API_REPO_URL } =
+  config;
