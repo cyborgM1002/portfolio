9127499ed083f1122060edc3d545552a6f6057ac
bugg-m <mkchauhan0543@gmail.com>
2024-03-21 19:30:37 +0530

    [PLO] P* Removing all apps

--- packages/portfolio-main/src/env.example.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  GITHUB_API_URL: "",
-  GITHUB_URL: "",
-  GITHUB_API_REPO_URL: "",
-  USER_SUMMARY: "",
-};
-
-export const { GITHUB_URL, USER_SUMMARY, GITHUB_API_URL, GITHUB_API_REPO_URL } =
-  config;
