c59ff90891043e605fc4b1434a6735cf6c4da539
bugg-m <mkchauhan0543@gmail.com>
2024-04-02 19:31:54 +0530

    [PLO] P*-06 Removed mobile navbar icons

---
+++ packages/portfolio-main/src/components/index.tsx
@@ -0,0 +1,9 @@
+export { default as bug } from "../../public/svg-logo/bug.svg";
+export { default as profile } from "../../public/images/profile.jpeg";
+export { default as Bubbles } from "./bubble-maker/Bubbles";
+export { default as LogoIcon } from "./logo-component/LogoIcon";
+export { default as FooterComponent } from "./footer/FooterComponent";
+export { default as NavbarComponent } from "./navbar/NavbarComponent";
+
+export { GITHUB_URL, GITHUB_API_URL, USER_SUMMARY } from "../env";
+export { CapitalizeAllLetter } from "../utils/utils";
