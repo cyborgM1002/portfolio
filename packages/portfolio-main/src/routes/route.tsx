c659d65ac293f3d8ef83ebcfd696fc590c542391
bugg-m <mkchauhan0543@gmail.com>
2024-04-02 19:00:00 +0530

    [PLO] P*-06 Created routes for mega-projects and mini react apps

--- packages/portfolio-main/src/routes/route.tsx
+++ packages/portfolio-main/src/routes/route.tsx
@@ -1,22 +1,30 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import { HomePage, ErrorPage, SkillsPage, ProjectsPage, ExperiencePage } from "../pages/index";
+import {
+  HomePage,
+  ErrorPage,
+  SkillsPage,
+  ProjectsHomePage,
+  ExperiencePage,
+  MegaProjectsHomePage,
+} from "../pages/index";
 import RootLayout from "../root.layout";
 import React from "react";
-import { MiniReactAppsHomePage } from "../../../react-mini-apps/src/bugg-react-apps";
+import MiniReactAppsHomePage from "../pages/projects/mini-react-apps-home-page/MiniReactAppsHomePage";
+import { PasskeyPage } from "../../../react-mini-apps/src/bugg-react-apps";
+
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
       <Route path='' element={<HomePage />} />
       <Route path='*' element={<ErrorPage />} />
       <Route path='skills' element={<SkillsPage />} />
-      <Route path='projects' element={<ProjectsPage />} />
+      <Route path='projects' element={<ProjectsHomePage />}>
+        <Route path='mega-projects' element={<MegaProjectsHomePage />} />
+        <Route path='react-apps' element={<MiniReactAppsHomePage />}>
+          <Route path='passkey-app' element={<PasskeyPage />} />
+        </Route>
+      </Route>
       <Route path='experience' element={<ExperiencePage />} />
-      <Route path='react-apps' element={<MiniReactAppsHomePage />}>
-        <Route path='test' element={<MiniReactAppsHomePage />} />
-      </Route>
-      <Route path='vue-apps' element={<MiniReactAppsHomePage />}>
-        <Route path='test' element={<MiniReactAppsHomePage />} />
-      </Route>
     </Route>,
   ),
 );
